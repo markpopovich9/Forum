d8fbd468eda8cf4d073057e00d7abcc0fce687ab
markpopovich9 <betmen070809@gmail.com>
2025-11-28 11:38:10 +0300

    html

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,52 @@
 import React from "react";
 
-export default function App() {
-    return <p>Вітаємо вас на сайті форума</p>;
+
+export  function App() {
+  return (
+    <>
+
+      <header>
+        <h1>Main Page</h1>
+      </header>
+
+
+      <main>
+          <nav>
+            <h2>FreeTalk</h2>
+
+            <ul>
+              <li>
+                <span>Posts</span>
+              </li>
+              <li>
+                <span>Profile</span>
+              </li>
+            </ul>
+          </nav>
+
+
+        <section>
+          <article>
+            <header>
+              <div>
+                
+              </div>
+              <span>19.3k</span>
+            </header>
+
+            <p>Hello this my dog</p>
+
+            
+              <img src="/images/dog.jpg"/>
+          </article>
+        </section>
+      </main>
+
+      
+      <footer>
+        <p>FreeTalk © 2025</p>
+      </footer>
+    </>
+  );
 }
+export default App;
